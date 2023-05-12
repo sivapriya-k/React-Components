6e393a867f1565a83fc81f49beb02c852748e5a4
sivapriya <shiva.priya87@gmail.com>
2023-05-11 21:23:22 -0700

    chore: Add Accordion Component

--- src/components/AccordionItem.jsx
+++ src/components/AccordionItem.jsx
@@ -1,7 +1,18 @@
-import {useState} from "react";
+import {useId, useState} from "react";
+
+function getAccordionHeaderId(accordionId, value) {
+    return accordionId + '-header-' + value;
+}
+
+function getAccordionPanelId(accordionId, value) {
+    return accordionId + '-panel-' + value;
+}
 
 export const AccordionItem = ({ title, content }) => {
     const [isOpen, setIsOpen] = useState(false)
+    const id = useId();
+    const headerId = getAccordionHeaderId(id, title)
+    const panelId = getAccordionPanelId(id, title)
 
     const toggleAccordion = () => {
         setIsOpen(!isOpen)
@@ -9,13 +20,13 @@ export const AccordionItem = ({ title, content }) => {
 
     return (
         <div className='accordion-item'>
-            <button onClick={toggleAccordion} className='accordion-item-button'>
+            <button onClick={toggleAccordion} className='accordion-item-button' id={headerId} aria-controls={panelId} aria-expanded={isOpen}>
                 {title}
-                <span className='accordion-icon'>{isOpen ? '-' : '+'}</span>
+                <span aria-hidden={true} className='accordion-icon'>{isOpen ? '-' : '+'}</span>
             </button>
-            {isOpen && <p className='accordion-item-contents'>
+            <p className='accordion-item-contents' aria-labelledBy={headerId} id={panelId} hidden={!isOpen}>
                 {content}
-            </p>}
+            </p>
         </div>
     )
 }
